f51c32112d0bfa8b04f3ad1a9baef345b9afcaad
Bruno Carvalho <f790698@CR7265ET714.corp.caixa.gov.br>
2019-05-28 10:51:21 -0300

    Projeto Inicial da API de gerenciamento do clube união
    Signed-off-by: Bruno Carvalho <f790698@CR7265ET714.corp.caixa.gov.br>

---
+++ src/main/java/com/bcconsulting/clubmanagersysapi/ClubmanagersysApiApplication.java
@@ -0,0 +1,13 @@
+package com.bcconsulting.clubmanagersysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubmanagersysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubmanagersysApiApplication.class, args);
+	}
+
+}
